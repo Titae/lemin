af31fcb216ec311d0879987fcdd1c425cbb55cab
Kevin Freire <kevin.freire@epitech.eu>
2017-12-16 02:12:53 +0100

    adding new fun in parser : config_to_list

--- include/parser.h
+++ include/parser.h
@@ -8,8 +8,11 @@
 #ifndef PARSER_H_
 #define PARSER_H_
 
+typedef struct list list_t;
+
 char **my_str_to_word_array(char *str);
 char **command_parser(char *command);
 char **env_var_to_word_array(char *env_var, char separator);
+list_t *config_to_list(char **file);
 
 #endif /* PARSER_H_ */
