662a501969c170304c8f73f11a2f0246e4151446
Kevin Freire <kevin.freire@epitech.eu>
2017-12-16 02:13:35 +0100

    adding new func in room : room create, room links and the struct room

--- include/room.h
+++ include/room.h
@@ -8,15 +8,19 @@
 #ifndef ROOM_H_
 #define ROOM_H_
 
+typedef struct list list_t;
 typedef struct room room_t;
 
 struct room
 {
  	char *name;
 	int nb_ant;
-	int is_start;
+	int is_special;
 	int *pos;
 	list_t *links;
 };
+
+room_t *room_create(char *name, int is_special, int x, int y);
+void create_link(char *name_room1, char *name_room2, list_t *room_list);
 
 #endif
