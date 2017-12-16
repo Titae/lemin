a69c45b769989fe713c730067083bab00e783724
Kevin Freire <kevin.freire@epitech.eu>
2017-12-16 02:15:38 +0100

    adding a function who create link between 2 rooms

--- src/utils/room/room_links.c
+++ src/utils/room/room_links.c
@@ -5,4 +5,20 @@
 ** create room links
 */
 
-void create_link(char *name_room1, char *name_room2)
+#include "room.h"
+#include "list.h"
+
+void create_link(char *name_room1, char *name_room2, list_t *room_list)
+{
+	list_t *list_room1 = get_node_by_name(&room_list, name_room1);
+	list_t *list_room2 = get_node_by_name(&room_list, name_room2);
+	room_t *room1;
+	room_t *room2;
+
+	if (list_room1 && list_room2) {
+		room1 = list_room1->data;
+		room2 = list_room2->data;
+		put_in_list(&(room1->links), room2);
+		put_in_list(&(room2->links), room1);
+	}
+}
